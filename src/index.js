6ae9e99d6747b069adf9172245e14412fa276f96
M. Mirmousavi <mirmousavi.m@gmail.com>
2020-12-29 00:45:04 +0330

    Add Redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import megaReducer from './reducers'
+
+const store = createStore(megaReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
